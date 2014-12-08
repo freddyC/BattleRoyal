6b3b9a6d1e7d8252798295ecb503ff085dc3bcbe
Fred Christensen <fred@ifit.com>
2014-12-07 21:16:46 -0700

    more little stuff

--- src/application/QueueMaster.java
+++ src/application/QueueMaster.java
@@ -1,5 +1,23 @@
 package application;
 
-public class QueueMaster {
+import java.util.Map;
+
+public class QueueMaster implements Watcher {
+	private Map players; 
+	
+	private static class InstanceHolder {
+		public static QueueMaster instance = new QueueMaster();
+	}
+	
+	public static QueueMaster getInstance() {
+		return InstanceHolder.instance;
+	}
+	
+	
+	
+	@Override
+	public void fire(String watchedName) {
+		
+	}
 
 }
